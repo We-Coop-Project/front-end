633aedcc60f3c809ff33e2031abdc180043d29b7
ayumitanaka <tanakayumi13@gmail.com>
2021-03-15 21:14:28 +0000

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import NavBar from "./components/UI/NavBar";
 import Home from "./components/Home/Home";
 
 const App = () => {
   return (
-    <>
-      <NavBar />
+    <Router>
+      <Route path="/" component={NavBar} />
+
       <div className="App">
-        <Home />
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+        </Switch>
       </div>
-    </>
+    </Router>
   );
 };
 
