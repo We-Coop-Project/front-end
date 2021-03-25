63883d2f95dd0655604188ca0b68f82dc70c7a38
ayumitanaka <tanakayumi13@gmail.com>
2021-03-25 22:05:53 +0000

    modified User.jsx

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -18,18 +18,18 @@ const User = () => {
   const [selectedGraph, setSelectedGraph] = useState("");
   const [graphData, setGraphData] = useState({});
   const [currentCompany, setCurrentCompany] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
   let uid = currentUser.uid;
 
   useEffect(() => {
     const getData = async () => {
-      setIsLoading(true);
+      // setIsLoading(true);
       const res = await api.get(`user_status/${uid}`);
       const calculatedData = await calculateData(res.data);
       setCurrentCompany(res.data.company_status);
       setGraphData(calculatedData);
-      setIsLoading(false);
+      // setIsLoading(false);
     };
     getData();
   }, [uid]);
@@ -57,9 +57,9 @@ const User = () => {
     }
   };
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  // if (isLoading) {
+  //   return <Loading />;
+  // }
 
   return (
     <div className="User">
